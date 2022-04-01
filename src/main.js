3d36d7f06fb71e65e7184c6d027bc1ea9655f9c8
Aswexx <a284ru8ccc@gmail.com>
2022-04-01 12:53:08 +0800

    add header and first step form

--- src/main.js
+++ src/main.js
@@ -1,3 +1,54 @@
 import './scss/main.scss'
 console.log('JS loaded')
-alert('hello');
+
+
+const modeToggle = document.querySelector('.mode-switcher');
+
+modeToggle.addEventListener('click', () => {
+  alert('ww');
+});
+
+
+//日夜模式
+const modeSwitcher = document.querySelector('.mode-switcher');
+const logo = document.querySelector('.logo')
+const body = document.querySelector('body')
+
+const header = document.querySelector('header')
+
+const changeImage = function () {
+  const imgPath =
+    body.classList.contains('darkmode') ? './image/logo-dark@2x.png' : './image/logo@2x.png';
+  const modeIconPath = 
+    body.classList.contains('darkmode') ? './image/sun-regular.svg#sun' : './image/moon-regular.svg#moon';
+    logo.src = imgPath
+  modeSwitcher.innerHTML = `<use href=${modeIconPath}></use>`
+  
+  const applyBackground = 
+  body.classList.contains('darkmode') ? '#121212' : '#ffffff';
+  header.style.backgroundColor = applyBackground
+}
+
+modeSwitcher.addEventListener('click', () => {
+  body.classList.toggle('darkmode')
+  changeImage()
+});
+
+//
+
+const navTrigger = document.querySelector('.nav-trigger-button');
+
+const toggleNavAndFeatures = function () {
+  const isNavTriggered = document.querySelector('.nav-trigger').checked;
+  if (isNavTriggered) {
+    header.style.backgroundColor = ''
+
+  } else {
+    const applyBackground = 
+    body.classList.contains('darkmode') ? '#121212' : '#ffffff';
+    header.style.backgroundColor = applyBackground
+  }
+}
+
+navTrigger.addEventListener('click', toggleNavAndFeatures);
+
